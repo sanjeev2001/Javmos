e1e4c93a04892556b6357d4fab819dc81b0566ef
Sanjeev Home <user@192.168.2.60>
2018-12-25 23:34:34 -0500

    zoom only works for factors of 800

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -1,6 +1,7 @@
 package javmos;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 
 public class CartesianPlane extends java.lang.Object {
 
@@ -11,18 +12,35 @@ public class CartesianPlane extends java.lang.Object {
     }
 
     public void drawPlane(java.awt.Graphics2D graphics2D) {
-        
-        for (int i = 0; i < 800 / 50; i++) {
-            graphics2D.drawLine(50 * i, 0, 50 * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            graphics2D.drawLine(0, 50 * i, 800, 50 * i);
+        int test = (int) gui.getZoom();
+        if (test == 50) {
+            for (int i = 0; i < 800 / 50; i++) {
+                graphics2D.drawLine(50 * i, 0, 50 * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
+                graphics2D.drawLine(0, 50 * i, 800, 50 * i);
+            }
+            graphics2D.setStroke(new BasicStroke(3));
+            graphics2D.drawLine(400, 0, 400, 800); //y-axis
+            graphics2D.drawLine(0, 400, 800, 400); //x-axis
+
+            for (int i = 0; i < 800 / 50; i++) {
+                graphics2D.drawLine(50 * i, 395, 50 * i, 405); 
+                graphics2D.drawLine(395, 50 * i, 405, 50 * i);
+            }
+        } else {
+            graphics2D.clearRect(0, 0, 800, 800);
+            for (int i = 0; i < 800 / test; i++) {
+                graphics2D.drawLine(test * i, 0, test * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
+                graphics2D.drawLine(0, test * i, 800, test * i);
+            }
+            graphics2D.setStroke(new BasicStroke(3));
+            graphics2D.drawLine(400, 0, 400, 800); //y-axis
+            graphics2D.drawLine(0, 400, 800, 400); //x-axis
+
+            for (int i = 0; i < 800 / test; i++) {
+                graphics2D.drawLine(test * i, 395, test * i, 405); 
+                graphics2D.drawLine(395, test * i, 405, test * i);
+            }
         }
-        graphics2D.setStroke(new BasicStroke(3));
-        graphics2D.drawLine(400, 0, 400, 800); //y-axis
-        graphics2D.drawLine(0, 400, 800, 400); //x-axis
-        
-        for (int i = 0; i < 800 / 50; i++) {
-            graphics2D.drawLine(50 * i, 395, 50 * i, 405); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            graphics2D.drawLine(395, 50 * i, 405, 50 * i);
-        }
+
     }
 }
