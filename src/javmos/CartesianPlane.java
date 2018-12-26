d444fac4f81547a7d25ec34fd232bb6fe62f06a7
Sanjeev Home <user@192.168.2.60>
2018-12-26 01:39:19 -0500

    grid completely done

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -18,12 +18,12 @@ public class CartesianPlane extends java.lang.Object {
         for (int i = 0; i < 800 / test; i++) {
             graphics2D.drawLine(400 - test * i, 0,400 - test * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
             graphics2D.drawLine(0,400 - test * i, 800,400 - test * i);
-        }
+        } // Draws all the horizontal lines in Q1 and Q2 and all vertical lines in Q2 and Q3
         
         for (int i = 0; i < 800 / test; i++) {
             graphics2D.drawLine(400 + test * i, 0,400 + test * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
             graphics2D.drawLine(0,400 + test * i, 800,400 + test * i);
-        }
+        } // Draws all the horizontal lines in Q3 and Q4 and all vertical lines in Q1 and Q4
         
         graphics2D.setStroke(new BasicStroke(3));
         graphics2D.drawLine(400, 0, 400, 800); //y-axis
@@ -32,11 +32,15 @@ public class CartesianPlane extends java.lang.Object {
         for (int i = 0; i < 800 / test; i++) {
             graphics2D.drawLine(400 - test * i, 395,400 - test * i, 405); //(x1, y1, x2, y2) draws thin vertical line every 50px
             graphics2D.drawLine(395,400 - test * i, 405,400 - test * i);
+            graphics2D.drawString(Integer.toString(-i), 405 - test * i, 399); // Draws horizontal axis numbers
+            graphics2D.drawString(Integer.toString(i), 405, 399 - test * i); // Draws horizontal axis numbers
         }
         
         for (int i = 0; i < 800 / test; i++) {
             graphics2D.drawLine(400 + test * i, 395,400 + test * i, 405); //(x1, y1, x2, y2) draws thin vertical line every 50px
             graphics2D.drawLine(395,400 + test * i, 405,400 + test * i);
+            graphics2D.drawString(Integer.toString(i), 405 + test * i, 399); // Draws horizontal axis numbers
+            graphics2D.drawString(Integer.toString(-i), 405, 399 + test * i); // Draws horizontal axis numbers
         }
 
     }
