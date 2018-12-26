76b6afebaf94385220142ac03fc07f9f3a8661b5
Sanjeev Home <user@192.168.2.60>
2018-12-26 13:08:15 -0500

    optimized the grid

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -1,7 +1,7 @@
 package javmos;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
+import java.awt.Font;
 
 public class CartesianPlane extends java.lang.Object {
 
@@ -14,34 +14,25 @@ public class CartesianPlane extends java.lang.Object {
     public void drawPlane(java.awt.Graphics2D graphics2D) {
         int test = (int) gui.getZoom();
         graphics2D.clearRect(0, 0, 800, 800);
-        
-        for (int i = 0; i < 800 / test; i++) {
-            graphics2D.drawLine(400 - test * i, 0,400 - test * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            graphics2D.drawLine(0,400 - test * i, 800,400 - test * i);
-        } // Draws all the horizontal lines in Q1 and Q2 and all vertical lines in Q2 and Q3
-        
-        for (int i = 0; i < 800 / test; i++) {
-            graphics2D.drawLine(400 + test * i, 0,400 + test * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            graphics2D.drawLine(0,400 + test * i, 800,400 + test * i);
-        } // Draws all the horizontal lines in Q3 and Q4 and all vertical lines in Q1 and Q4
-        
-        graphics2D.setStroke(new BasicStroke(3));
-        graphics2D.drawLine(400, 0, 400, 800); //y-axis
-        graphics2D.drawLine(0, 400, 800, 400); //x-axis
+        graphics2D.setFont(new Font("TimesRoman", Font.BOLD, 14)); //Sets font size and font type
 
         for (int i = 0; i < 800 / test; i++) {
-            graphics2D.drawLine(400 - test * i, 395,400 - test * i, 405); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            graphics2D.drawLine(395,400 - test * i, 405,400 - test * i);
+            graphics2D.setStroke(new BasicStroke(1)); //Sets thin line stroke
+            graphics2D.drawLine(400 - test * i, 0, 400 - test * i, 800); //(x1, y1, x2, y2) draws thin vertical lines 
+            graphics2D.drawLine(0, 400 - test * i, 800, 400 - test * i);//(x1, y1, x2, y2) draws thin horizontal line 
+            graphics2D.drawLine(400 + test * i, 0, 400 + test * i, 800); //(x1, y1, x2, y2) draws thin vertical line 
+            graphics2D.drawLine(0, 400 + test * i, 800, 400 + test * i);//(x1, y1, x2, y2) draws thin horizontal line 
+            graphics2D.setStroke(new BasicStroke(3));//Sets thick line stroke
+            graphics2D.drawLine(400, 0, 400, 800); //y-axis
+            graphics2D.drawLine(0, 400, 800, 400); //x-axis
+            graphics2D.drawLine(400 - test * i, 395, 400 - test * i, 405); //Draws axis tick marks
+            graphics2D.drawLine(395, 400 - test * i, 405, 400 - test * i);//Draws axis tick marks
+            graphics2D.drawLine(400 + test * i, 395, 400 + test * i, 405); //Draws axis tick marks
+            graphics2D.drawLine(395, 400 + test * i, 405, 400 + test * i);//Draws axis tick marks
             graphics2D.drawString(Integer.toString(-i), 405 - test * i, 399); // Draws negative horizontal axis numbers
             graphics2D.drawString(Integer.toString(i), 405, 399 - test * i); // Draws positive vertical axis numbers
-        }
-        
-        for (int i = 0; i < 800 / test; i++) {
-            graphics2D.drawLine(400 + test * i, 395,400 + test * i, 405); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            graphics2D.drawLine(395,400 + test * i, 405,400 + test * i);
             graphics2D.drawString(Integer.toString(i), 405 + test * i, 399); // Draws positive horizontal axis numbers
             graphics2D.drawString(Integer.toString(-i), 405, 399 + test * i); // Draws negative vertical axis numbers
-        }
-
+        } 
     }
 }
