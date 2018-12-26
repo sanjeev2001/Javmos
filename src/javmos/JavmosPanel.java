462a73797f431eb70ecd3b0e0f39554f8e7f6fa5
Sanjeev Home <>
2018-12-25 20:42:05 -0500

    grid done except axis numbers

--- src/javmos/JavmosPanel.java
+++ src/javmos/JavmosPanel.java
@@ -36,16 +36,7 @@ public class JavmosPanel extends JPanel {
 
     @Override
     public void paintComponent(Graphics graphics) {
-        Graphics2D thinLine = (Graphics2D) graphics;
-        Graphics2D boldLine = (Graphics2D) graphics;
-
-        for (int i = 0; i < 800 / 50; i++) {
-            thinLine.drawLine(50 * i, 0, 50 * i, 800); //(x1, y1, x2, y2) draws thin vertical line every 50px
-            thinLine.drawLine(0, 50 * i, 800, 50 * i);
-        }
-
-        boldLine.setStroke(new BasicStroke(3));
-        boldLine.drawLine(400, 0, 400, 800); //y-axis
-        boldLine.drawLine(0, 400, 800, 400); //x-axis
+        plane = new CartesianPlane(gui);
+        plane.drawPlane((Graphics2D) graphics);
     }
 }
