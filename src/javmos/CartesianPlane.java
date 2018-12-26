2e0ddf36feef4f2e1adf2e59a41a506f30c80712
Sanjeev Home <user@192.168.2.60>
2018-12-26 13:34:52 -0500

    implemented domain and range

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -12,27 +12,28 @@ public class CartesianPlane extends java.lang.Object {
     }
 
     public void drawPlane(java.awt.Graphics2D graphics2D) {
-        int test = (int) gui.getZoom();
+        int pixel = (int) gui.getZoom();
+        int d = (int) gui.getDomainStep();
         graphics2D.clearRect(0, 0, 800, 800);
         graphics2D.setFont(new Font("TimesRoman", Font.BOLD, 14)); //Sets font size and font type
 
-        for (int i = 0; i < 800 / test; i++) {
+        for (int i = 0; i < 800 / pixel; i++) {
             graphics2D.setStroke(new BasicStroke(1)); //Sets thin line stroke
-            graphics2D.drawLine(400 - test * i, 0, 400 - test * i, 800); //(x1, y1, x2, y2) draws thin vertical lines 
-            graphics2D.drawLine(0, 400 - test * i, 800, 400 - test * i);//(x1, y1, x2, y2) draws thin horizontal line 
-            graphics2D.drawLine(400 + test * i, 0, 400 + test * i, 800); //(x1, y1, x2, y2) draws thin vertical line 
-            graphics2D.drawLine(0, 400 + test * i, 800, 400 + test * i);//(x1, y1, x2, y2) draws thin horizontal line 
+            graphics2D.drawLine(400 - pixel * i, 0, 400 - pixel * i, 800); //(x1, y1, x2, y2) draws thin vertical lines 
+            graphics2D.drawLine(0, 400 - pixel * i, 800, 400 - pixel * i);//(x1, y1, x2, y2) draws thin horizontal line 
+            graphics2D.drawLine(400 + pixel * i, 0, 400 + pixel * i, 800); //(x1, y1, x2, y2) draws thin vertical line 
+            graphics2D.drawLine(0, 400 + pixel * i, 800, 400 + pixel * i);//(x1, y1, x2, y2) draws thin horizontal line 
             graphics2D.setStroke(new BasicStroke(3));//Sets thick line stroke
             graphics2D.drawLine(400, 0, 400, 800); //y-axis
             graphics2D.drawLine(0, 400, 800, 400); //x-axis
-            graphics2D.drawLine(400 - test * i, 395, 400 - test * i, 405); //Draws axis tick marks
-            graphics2D.drawLine(395, 400 - test * i, 405, 400 - test * i);//Draws axis tick marks
-            graphics2D.drawLine(400 + test * i, 395, 400 + test * i, 405); //Draws axis tick marks
-            graphics2D.drawLine(395, 400 + test * i, 405, 400 + test * i);//Draws axis tick marks
-            graphics2D.drawString(Integer.toString(-i), 405 - test * i, 399); // Draws negative horizontal axis numbers
-            graphics2D.drawString(Integer.toString(i), 405, 399 - test * i); // Draws positive vertical axis numbers
-            graphics2D.drawString(Integer.toString(i), 405 + test * i, 399); // Draws positive horizontal axis numbers
-            graphics2D.drawString(Integer.toString(-i), 405, 399 + test * i); // Draws negative vertical axis numbers
+            graphics2D.drawLine(400 - pixel * i, 395, 400 - pixel * i, 405); //Draws axis tick marks
+            graphics2D.drawLine(395, 400 - pixel * i, 405, 400 - pixel * i);//Draws axis tick marks
+            graphics2D.drawLine(400 + pixel * i, 395, 400 + pixel * i, 405); //Draws axis tick marks
+            graphics2D.drawLine(395, 400 + pixel * i, 405, 400 + pixel * i);//Draws axis tick marks
+            graphics2D.drawString(Integer.toString(-i * d), 405 - pixel * i, 399); // Draws negative horizontal axis numbers
+            graphics2D.drawString(Integer.toString(i * d), 405, 399 - pixel * i); // Draws positive vertical axis numbers
+            graphics2D.drawString(Integer.toString(i * d), 405 + pixel * i, 399); // Draws positive horizontal axis numbers
+            graphics2D.drawString(Integer.toString(-i * d), 405, 399 + pixel * i); // Draws negative vertical axis numbers
         } 
     }
 }
