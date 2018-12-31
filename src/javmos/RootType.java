cbb629475acf6bb7241c3d4a5d62ce4f2ba0ba30
Farhan Faruqui <farhanfaruqui13@gmail.com>
2018-12-31 12:31:57 -0500

    Completed RootType class

--- src/javmos/RootType.java
+++ src/javmos/RootType.java
@@ -3,18 +3,23 @@ package javmos;
 import java.awt.Color;
 
 public enum RootType {
-    X_INTERCEPT(Color.GREEN),
-    CRITICAL_POINT(Color.RED),
-    INFLECTION_POINT(Color.BLUE);
+    X_INTERCEPT(Color.GREEN, "x-intercept"),
+    CRITICAL_POINT(Color.RED, "Critical Point"),
+    INFLECTION_POINT(Color.BLUE, "Inflection Point");
 
     public final Color color;
+    public final String name;
 
-    RootType(Color color) {
+    RootType(Color color, String name) {
         this.color = color;
+        this.name = name;
     }
     
     public Color getPointColor() {
-        // Complete me
-        return Color.BLACK;
+        return color;
+    }
+
+    public String getPointName() {
+        return name;
     }
 }
