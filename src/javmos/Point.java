153a65ad322d111523eae4f70061293c0c1fffe4
Farhan Faruqui <farhanfaruqui13@gmail.com>
2018-12-31 12:54:18 -0500

    Filled out missing instance variables and methods for Point class

--- src/javmos/Point.java
+++ src/javmos/Point.java
@@ -1,15 +1,50 @@
 package javmos;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.geom.Ellipse2D;
 
 public class Point {
 
-    public final int RADIUS = 5;
+    public final JavmosGUI gui;
     public Ellipse2D.Double point;
+    public final int RADIUS = 5;
+    public final RootType rootType;
+    public double x;
+    public double y;
+
+    public Point(JavmosGUI gui, RootType type, double x, double y) {
+        this.gui = gui;
+        this.rootType = type;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void drawPoint(Graphics2D graphics2D) {
+
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // Complete me
+        return false;
+    }
+
+    public Ellipse2D.Double getPoint() {
+        return point;
+    }
 
     public RootType getRootType() {
         // Complete me
-        return null;
+        return rootType;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 
     @Override
@@ -18,19 +53,9 @@ public class Point {
         return 0;
     }
 
-   @Override
-    public boolean equals(Object object) {
-        // Complete me
-        return false;
-    }
-
     @Override
     public String toString() {
-        // Complete me
-        return "";
+        return rootType.name + "(" + String.valueOf(x) + "," + String.valueOf(y) + ")";
     }
-    
-    public Ellipse2D.Double getPoint() {
-        return point;
-    }
+
 }
