3683cb0dfe6203945b4266684e417efbd5ea29e6
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-01-27 18:13:27 -0500

    pushing whatever i have

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -16,9 +16,16 @@ public class CartesianPlane extends java.lang.Object {
         int pixel = (int) gui.getZoom();
         double d = gui.getDomainStep();
         double r = gui.getRangeStep();
+        int scaleFactor = 1;
         DecimalFormat thousandth = new DecimalFormat("#.###");
         graphics2D.clearRect(0, 0, 800, 800);
-        graphics2D.setFont(new Font("TimesRoman", Font.BOLD, 14)); //Sets font size and font type
+        graphics2D.setFont(new Font("TimesRoman", Font.PLAIN, 14)); //Sets font size and font type
+
+        if (gui.getZoom() == 30.0 || gui.getZoom() == 20.0) {
+            scaleFactor = 2;
+        } else if (gui.getZoom() == 10.0) {
+            scaleFactor = 5;
+        }
 
         for (int i = 0; i < 800 / pixel; i++) {
             graphics2D.setStroke(new BasicStroke(1)); //Sets thin line stroke
@@ -33,30 +40,23 @@ public class CartesianPlane extends java.lang.Object {
             graphics2D.drawLine(395, 400 - pixel * i, 405, 400 - pixel * i);//Draws axis tick marks
             graphics2D.drawLine(400 + pixel * i, 395, 400 + pixel * i, 405); //Draws axis tick marks
             graphics2D.drawLine(395, 400 + pixel * i, 405, 400 + pixel * i);//Draws axis tick marks
-            if (gui.getZoom() != 10) {
-                //Draws horizontal axis numbers
-                if ((gui.getDomainStep() % 1) == 0) {
-                    graphics2D.drawString(String.valueOf((int) (-i * d)), 405 - pixel * i, 399);
-                    graphics2D.drawString(String.valueOf((int) (i * d)), 405 + pixel * i, 399);
-                } else {
-                    graphics2D.drawString(String.valueOf(thousandth.format(-i * d)), 405 - pixel * i, 399);
-                    graphics2D.drawString(String.valueOf(thousandth.format(i * d)), 405 + pixel * i, 399);
-                }
-
-                // Draws vertical axis numbers
-                if ((gui.getRangeStep() % 1) == 0) {
-                    graphics2D.drawString(String.valueOf((int) (-i * r)), 405, 399 + pixel * i);
-                    graphics2D.drawString(String.valueOf((int) (i * r)), 405, 399 - pixel * i);
-                } else {
-                    graphics2D.drawString(String.valueOf(thousandth.format(-i * r)), 405, 399 + pixel * i);
-                    graphics2D.drawString(String.valueOf(thousandth.format(i * r)), 405, 399 - pixel * i);
-                }
-            } else { // Spaces out the axis numbers at 10x so that it's readable
-                graphics2D.drawString(Double.toString(-2 * i * d), 405 - pixel * 2 * i, 399); // Draws negative horizontal axis numbers
-                graphics2D.drawString(Double.toString(2 * i * r), 405, 399 - pixel * 2 * i); // Draws positive vertical axis numbers
-                graphics2D.drawString(Double.toString(2 * i * d), 405 + pixel * 2 * i, 399); // Draws positive horizontal axis numbers
-                graphics2D.drawString(Double.toString(-2 * i * r), 405, 399 + pixel * 2 * i); // Draws negative vertical axis numbers
+            //Draws horizontal axis numbers
+            if ((gui.getDomainStep() % 1) == 0) {
+                graphics2D.drawString(String.valueOf((int) (-i * d * scaleFactor)), 405 - pixel * i * scaleFactor, 399);
+                graphics2D.drawString(String.valueOf((int) (i * d * scaleFactor)), 405 + pixel * i * scaleFactor, 399);
+            } else {
+                graphics2D.drawString(String.valueOf(thousandth.format(-i * d)), 405 - pixel * i, 399);
+                graphics2D.drawString(String.valueOf(thousandth.format(i * d)), 405 + pixel * i, 399);
             }
-        } 
+
+            // Draws vertical axis numbers
+            if ((gui.getRangeStep() % 1) == 0) {
+                graphics2D.drawString(String.valueOf((int) (-i * r)), 405, 399 + pixel * i);
+                graphics2D.drawString(String.valueOf((int) (i * r)), 405, 399 - pixel * i);
+            } else {
+                graphics2D.drawString(String.valueOf(thousandth.format(-i * r)), 405, 399 + pixel * i);
+                graphics2D.drawString(String.valueOf(thousandth.format(i * r)), 405, 399 - pixel * i);
+            }
+        }
     }
 }
