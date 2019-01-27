9a7b25faf0646acb348a8f347372dce1789cc03c
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-01-27 17:07:19 -0500

    if label is an int shows up as 1 vs 1.0

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -13,8 +13,8 @@ public class CartesianPlane extends java.lang.Object {
 
     public void drawPlane(java.awt.Graphics2D graphics2D) {
         int pixel = (int) gui.getZoom();
-        double d = (int) gui.getDomainStep();
-        double r = (int) gui.getRangeStep();
+        double d = gui.getDomainStep();
+        double r = gui.getRangeStep();
         graphics2D.clearRect(0, 0, 800, 800);
         graphics2D.setFont(new Font("TimesRoman", Font.BOLD, 14)); //Sets font size and font type
 
@@ -32,15 +32,28 @@ public class CartesianPlane extends java.lang.Object {
             graphics2D.drawLine(400 + pixel * i, 395, 400 + pixel * i, 405); //Draws axis tick marks
             graphics2D.drawLine(395, 400 + pixel * i, 405, 400 + pixel * i);//Draws axis tick marks
             if (gui.getZoom() != 10) {
-            graphics2D.drawString(Double.toString(-i * d), 405 - pixel * i, 399); // Draws negative horizontal axis numbers
-            graphics2D.drawString(Double.toString(i * r), 405, 399 - pixel * i); // Draws positive vertical axis numbers
-            graphics2D.drawString(Double.toString(i * d), 405 + pixel * i, 399); // Draws positive horizontal axis numbers
-            graphics2D.drawString(Double.toString(-i * r), 405, 399 + pixel * i); // Draws negative vertical axis numbers
+                //Draws horizontal axis numbers
+                if ((gui.getDomainStep() % 1) == 0) {
+                    graphics2D.drawString(String.valueOf((int) (-i * d)), 405 - pixel * i, 399);
+                    graphics2D.drawString(String.valueOf((int) (i * d)), 405 + pixel * i, 399);
+                } else {
+                    graphics2D.drawString(String.valueOf(-i * d), 405 - pixel * i, 399);
+                    graphics2D.drawString(String.valueOf(i * d), 405 + pixel * i, 399);
+                }
+
+                // Draws vertical axis numbers
+                if ((gui.getRangeStep() % 1) == 0) {
+                    graphics2D.drawString(String.valueOf((int) (-i * r)), 405, 399 + pixel * i);
+                    graphics2D.drawString(String.valueOf((int) (i * r)), 405, 399 - pixel * i);
+                } else {
+                    graphics2D.drawString(String.valueOf(-i * r), 405, 399 + pixel * i);
+                    graphics2D.drawString(String.valueOf(i * r), 405, 399 - pixel * i);
+                }
             } else { // Spaces out the axis numbers at 10x so that it's readable
-            graphics2D.drawString(Double.toString(-2*i * d), 405 - pixel * 2*i, 399); // Draws negative horizontal axis numbers
-            graphics2D.drawString(Double.toString(2*i * r), 405, 399 - pixel * 2*i); // Draws positive vertical axis numbers
-            graphics2D.drawString(Double.toString(2*i * d), 405 + pixel * 2*i, 399); // Draws positive horizontal axis numbers
-            graphics2D.drawString(Double.toString(-2*i * r), 405, 399 + pixel * 2*i); // Draws negative vertical axis numbers
+                graphics2D.drawString(Double.toString(-2 * i * d), 405 - pixel * 2 * i, 399); // Draws negative horizontal axis numbers
+                graphics2D.drawString(Double.toString(2 * i * r), 405, 399 - pixel * 2 * i); // Draws positive vertical axis numbers
+                graphics2D.drawString(Double.toString(2 * i * d), 405 + pixel * 2 * i, 399); // Draws positive horizontal axis numbers
+                graphics2D.drawString(Double.toString(-2 * i * r), 405, 399 + pixel * 2 * i); // Draws negative vertical axis numbers
             }
         } 
     }
