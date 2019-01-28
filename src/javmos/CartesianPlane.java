83f034a8ea609b0a273dd208ee77618e0747ea42
Sanjeev <Sanjeev@192.168.2.14>
2019-01-27 19:14:39 -0500

    axis numbers bug

--- src/javmos/CartesianPlane.java
+++ src/javmos/CartesianPlane.java
@@ -1,6 +1,7 @@
 package javmos;
 
 import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.Font;
 import java.text.DecimalFormat;
 
@@ -28,12 +29,14 @@ public class CartesianPlane extends java.lang.Object {
         }
 
         for (int i = 0; i < 800 / pixel; i++) {
+            graphics2D.setColor(Color.GRAY);
             graphics2D.setStroke(new BasicStroke(1)); //Sets thin line stroke
             graphics2D.drawLine(400 - pixel * i, 0, 400 - pixel * i, 800); //(x1, y1, x2, y2) draws thin vertical lines 
             graphics2D.drawLine(0, 400 - pixel * i, 800, 400 - pixel * i);//(x1, y1, x2, y2) draws thin horizontal line 
             graphics2D.drawLine(400 + pixel * i, 0, 400 + pixel * i, 800); //(x1, y1, x2, y2) draws thin vertical line 
             graphics2D.drawLine(0, 400 + pixel * i, 800, 400 + pixel * i);//(x1, y1, x2, y2) draws thin horizontal line 
             graphics2D.setStroke(new BasicStroke(3));//Sets thick line stroke
+            graphics2D.setColor(Color.BLACK);
             graphics2D.drawLine(400, 0, 400, 800); //y-axis
             graphics2D.drawLine(0, 400, 800, 400); //x-axis
             graphics2D.drawLine(400 - pixel * i, 395, 400 - pixel * i, 405); //Draws axis tick marks
@@ -42,20 +45,26 @@ public class CartesianPlane extends java.lang.Object {
             graphics2D.drawLine(395, 400 + pixel * i, 405, 400 + pixel * i);//Draws axis tick marks
             //Draws horizontal axis numbers
             if ((gui.getDomainStep() % 1) == 0) {
-                graphics2D.drawString(String.valueOf((int) (-i * d * scaleFactor)), 405 - pixel * i * scaleFactor, 399);
-                graphics2D.drawString(String.valueOf((int) (i * d * scaleFactor)), 405 + pixel * i * scaleFactor, 399);
+                graphics2D.setColor(Color.WHITE);
+                graphics2D.fillRect(390 - pixel * (i+1) * scaleFactor, 410, 15, 10);
+                graphics2D.setColor(Color.BLACK);
+                graphics2D.drawString(String.valueOf((int) (-(i+1) * d * scaleFactor)), 392 - pixel * (i+1) * scaleFactor, 420);
+                graphics2D.drawString(String.valueOf((int) ((i+1) * d * scaleFactor)), 397 + pixel * (i+1) * scaleFactor, 420);
             } else {
-                graphics2D.drawString(String.valueOf(thousandth.format(-i * d)), 405 - pixel * i, 399);
-                graphics2D.drawString(String.valueOf(thousandth.format(i * d)), 405 + pixel * i, 399);
+                graphics2D.setColor(Color.WHITE);
+                graphics2D.fillRect(390 - pixel * (i+1) * scaleFactor, 410, 15, 10);
+                graphics2D.setColor(Color.BLACK);
+                graphics2D.drawString(String.valueOf(thousandth.format((-(i+1)) * d)), 392 - pixel * (i+1), 420);
+                graphics2D.drawString(String.valueOf(thousandth.format((i+1) * d)), 397 + pixel * (i+1), 420);
             }
 
             // Draws vertical axis numbers
             if ((gui.getRangeStep() % 1) == 0) {
-                graphics2D.drawString(String.valueOf((int) (-i * r)), 405, 399 + pixel * i);
-                graphics2D.drawString(String.valueOf((int) (i * r)), 405, 399 - pixel * i);
+                graphics2D.drawString(String.valueOf((int) (-(i+1) * r)), 385, 405 + pixel * (i+1));
+                graphics2D.drawString(String.valueOf((int) ((i+1) * r)), 385, 405 - pixel * (i+1));
             } else {
-                graphics2D.drawString(String.valueOf(thousandth.format(-i * r)), 405, 399 + pixel * i);
-                graphics2D.drawString(String.valueOf(thousandth.format(i * r)), 405, 399 - pixel * i);
+                graphics2D.drawString(String.valueOf(thousandth.format(-(i+1) * r)), 382, 405 + pixel * (i+1));
+                graphics2D.drawString(String.valueOf(thousandth.format((i+1) * r)), 385, 405 - pixel * (i+1));
             }
         }
     }
