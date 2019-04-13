c97e76856610ded3aec5c65fd57a61e17d3545b1
Sanjeev <Sanjeev@192.168.2.14>
2019-04-13 17:24:04 -0400

    fixed trig parsing

--- src/javmos/components/functions/Trigonometric.java
+++ src/javmos/components/functions/Trigonometric.java
@@ -20,20 +20,20 @@ public abstract class Trigonometric extends Function {
         super(gui);
         if (function.charAt(0) == name.charAt(0)){
             a = 1;
-        } else if (function.charAt(0) == '-') {
+        } else if (function.charAt(0) == '-' && function.charAt(1) == name.charAt(0)) {
             a = -1;
         } else if (function.charAt(0) != function.charAt(function.indexOf(Character.toString(name.charAt(0))) - 1)) {
-            a = Integer.parseInt(Character.toString(function.charAt(0)) + Character.toString(function.charAt(function.indexOf(Character.toString(name.charAt(0))) - 1)));
+            a = Integer.parseInt(function.substring(0, function.indexOf(Character.toString(name.charAt(0)))));
         } else {
             a = Integer.parseInt(Character.toString(function.charAt(0)));
         }
         
         if (function.charAt(function.indexOf("(") + 1) == 'x') {
             k = 1;
-        } else if (function.charAt(function.indexOf("(") + 1) == '-') {
+        } else if (function.charAt(function.indexOf("(") + 1) == '-' && function.charAt(function.indexOf("(") + 1) == function.charAt(function.indexOf(Character.toString('x')))) {
             k = -1;
         } else if (function.charAt(function.indexOf("(") + 1) != function.charAt(function.indexOf("x") - 1)) {
-            k = Integer.parseInt(Character.toString(function.charAt((function.indexOf("(") + 1))) + Character.toString(function.charAt(function.indexOf("x") - 1)));
+            k = Integer.parseInt(function.substring(function.indexOf("(") + 1, function.indexOf(Character.toString('x'))));
         } else {
             k = Integer.parseInt(Character.toString(function.charAt(function.indexOf("(") + 1)));
         }
