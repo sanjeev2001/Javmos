14163e4117deb0a55a0ed17ec6d6739ae1b993cc
Sanjeev <Sanjeev@192.168.2.14>
2019-04-13 17:08:16 -0400

    parsed trig functions

--- src/javmos/components/functions/Trigonometric.java
+++ src/javmos/components/functions/Trigonometric.java
@@ -18,7 +18,25 @@ public abstract class Trigonometric extends Function {
 
     public Trigonometric(JavmosGUI gui, String function, String name) {
         super(gui);
+        if (function.charAt(0) == name.charAt(0)){
+            a = 1;
+        } else if (function.charAt(0) == '-') {
+            a = -1;
+        } else if (function.charAt(0) != function.charAt(function.indexOf(Character.toString(name.charAt(0))) - 1)) {
+            a = Integer.parseInt(Character.toString(function.charAt(0)) + Character.toString(function.charAt(function.indexOf(Character.toString(name.charAt(0))) - 1)));
+        } else {
+            a = Integer.parseInt(Character.toString(function.charAt(0)));
+        }
         
+        if (function.charAt(function.indexOf("(") + 1) == 'x') {
+            k = 1;
+        } else if (function.charAt(function.indexOf("(") + 1) == '-') {
+            k = -1;
+        } else if (function.charAt(function.indexOf("(") + 1) != function.charAt(function.indexOf("x") - 1)) {
+            k = Integer.parseInt(Character.toString(function.charAt((function.indexOf("(") + 1))) + Character.toString(function.charAt(function.indexOf("x") - 1)));
+        } else {
+            k = Integer.parseInt(Character.toString(function.charAt(function.indexOf("(") + 1)));
+        }
     }
 
 }
