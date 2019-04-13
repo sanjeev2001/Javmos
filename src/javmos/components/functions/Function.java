fadc0f10a2b520f11e8b7d8ea7b9409a229fdde5
Sanjeev <Sanjeev@192.168.2.14>
2019-04-13 14:55:21 -0400

    added draw and getvalueat

--- src/javmos/components/functions/Function.java
+++ src/javmos/components/functions/Function.java
@@ -5,8 +5,11 @@
  */
 package javmos.components.functions;
 
+import java.awt.BasicStroke;
+import java.awt.geom.Line2D;
 import javmos.JavmosGUI;
 import javmos.components.JavmosComponent;
+import javmos.enums.FunctionType;
 
 /**
  *
@@ -19,7 +22,21 @@ public abstract class Function extends JavmosComponent {
     }
     
     public void draw(java.awt.Graphics2D graphics2D) {
-        
+        /*for (double i = gui.getMinDomain(); i < gui.getMaxDomain(); i += 0.01) {
+            if (getValueAt(i) <= gui.getMaxRange() && getValueAt(i) >= gui.getMinRange()) {//only draws within the given max and min range
+                double x1 = i * gui.getZoom() / gui.getDomainStep();
+                double y1 = getValueAt(i) * gui.getZoom() / gui.getRangeStep();
+                double x2 = (i + 0.01) * gui.getZoom() / gui.getDomainStep();
+                double y2 = getValueAt(i + 0.01) * gui.getZoom() / gui.getRangeStep();
+                graphics2D.setStroke(new BasicStroke(3));
+                /*400 added to x1 and x2 in order to start drawing from the origin 
+                y1 and y2 are subtracted from 400 in order to start drawing from the origin 
+                graphics2D.draw(new Line2D.Double(400 + x1, 400 - y1, 400 + x2, 400 - y2));
+            }
+        }*/
     }
+    
+    public abstract double getValueAt(double x, FunctionType functionType);
+    
 
 }
