f11719f1e16747bf86e7e4c91254504c344e54fb
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-04-14 13:47:23 -0400

    String input

--- src/javmos/components/functions/Logarithmic.java
+++ src/javmos/components/functions/Logarithmic.java
@@ -5,7 +5,6 @@
  */
 package javmos.components.functions;
 
-import java.awt.Graphics2D;
 import javmos.JavmosGUI;
 import javmos.enums.FunctionType;
 
@@ -19,8 +18,36 @@ public class Logarithmic extends Function {
    public double base;
    public double k;
 
-   public Logarithmic(JavmosGUI gui, String function){
-       super(gui);
+    public Logarithmic(JavmosGUI gui, String function) {
+        super(gui);
+        function = function.contains("=") ? function.substring(function.indexOf("=") + 1, function.length()) : function;
+        double a;
+        double base;
+        double k;
+
+        if (function.substring(0, 1).equals("l")) {
+            a = 1;
+        } else if (function.substring(0, 1).equals("-l")) {
+            a = -1;
+        } else {
+            a = Double.parseDouble(function.substring(0, function.indexOf("l")));
+        }
+
+        if (function.contains("ln")) {
+            base = Math.E;
+        } else if (function.contains("g(")) {
+            base = 10;
+        } else {
+            base = Double.parseDouble(function.substring(function.indexOf("g") + 1, function.indexOf("(")));
+        }
+
+        if (function.contains("(x")) {
+            k = 1;
+        } else if (function.contains("(-x")) {
+            k = -1;
+        } else {
+            k = Double.parseDouble(function.substring(function.indexOf("(") + 1, function.indexOf("x")));
+        }
     }
 
     @Override
