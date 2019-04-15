235e6d9e9c99195646120177e75ebe4f56861278
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-04-14 23:23:27 -0400

    Finished most of getValue

--- src/javmos/components/functions/Logarithmic.java
+++ src/javmos/components/functions/Logarithmic.java
@@ -6,6 +6,7 @@
 package javmos.components.functions;
 
 import javmos.JavmosGUI;
+import javmos.components.Point;
 import javmos.enums.FunctionType;
 
 
@@ -50,8 +51,33 @@ public class Logarithmic extends Function {
         }
     }
 
+    public java.lang.String getFirstDerivative() {
+        return base == Math.E ? "f'(x) = " + a + "/" + "x" : "f'(x) = " + a + "/" + "(xln" + base + ")";
+    }
+
+    public java.lang.String getSecondDerivative() {
+        String sign = a > 0 ? "-" : "";
+        return base == Math.E ? "f'(x) = " + sign + a + "/" + "x^2" : "f'(x) = " + sign + a + "/" + "(x^2ln" + base + ")";
+    }
+
+    public java.util.HashSet<Point> getCriticalPoints() {
+        return null;
+    }
+
+    public java.util.HashSet<Point> getInflectionPoints() {
+        return null;
+    }
+
     @Override
     public double getValueAt(double x, FunctionType functionType) {
-        return 0.0;
+        if (functionType == FunctionType.FIRST_DERIVATIVE) {
+            return a / (x * Math.log(base));
+        } else if (functionType == FunctionType.SECOND_DERIVATIVE) {
+            return a > 0 ? -a / (Math.pow(x, 2) * Math.log(base)) : a / (Math.pow(x, 2) * Math.log(base));
+        } else if (functionType == FunctionType.THIRD_DERIVATIVE) {
+            return 0;
+        } else {
+            return a * (Math.log10(k * x) / Math.log10(base));
+        }
     }
 }
