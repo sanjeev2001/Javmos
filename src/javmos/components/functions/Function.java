fb105c5fa25c805754cce160a22eddea968d1fa1
Sanjeev <Sanjeev@10.10.129.27>
2019-04-29 22:20:12 -0400

    fixed drawing vertical tangents

--- src/javmos/components/functions/Function.java
+++ src/javmos/components/functions/Function.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javmos.components.functions;
 
 import java.awt.BasicStroke;
@@ -20,40 +15,47 @@ import javmos.enums.RootType;
  * @author user
  */
 public abstract class Function extends JavmosComponent {
-    
+
     protected Function(JavmosGUI gui) {
         super(gui);
     }
-    
+
+    @Override
     public void draw(java.awt.Graphics2D graphics2D) {
+        double x1, x2, y1, y2;
         for (double i = gui.getMinDomain(); i < gui.getMaxDomain(); i += 0.01) {
             if (getValueAt(i, FunctionType.ORIGINAL) <= gui.getMaxRange() && getValueAt(i, FunctionType.ORIGINAL) >= gui.getMinRange()) {//only draws within the given max and min range
-                double x1 = i * gui.getZoom() / gui.getDomainStep();
-                double y1 = getValueAt(i, FunctionType.ORIGINAL) * gui.getZoom() / gui.getRangeStep();
-                double x2 = (i + 0.01) * gui.getZoom() / gui.getDomainStep();
-                double y2 = getValueAt(i + 0.01, FunctionType.ORIGINAL) * gui.getZoom() / gui.getRangeStep();
+                x1 = i * gui.getZoom() / gui.getDomainStep();
+                y1 = getValueAt(i, FunctionType.ORIGINAL) * gui.getZoom() / gui.getRangeStep();
+                x2 = (i + 0.01) * gui.getZoom() / gui.getDomainStep();
+                y2 = getValueAt(i + 0.01, FunctionType.ORIGINAL) * gui.getZoom() / gui.getRangeStep();
                 graphics2D.setStroke(new BasicStroke(3));
                 graphics2D.setColor(Color.black);
-                graphics2D.draw(new Line2D.Double(400 + x1, 400 - y1, 400 + x2, 400 - y2));
+                Line2D.Double line = new Line2D.Double(400 + x1, 400 - y1, 400 + x2, 400 - y2);
+
+                if (line.getY1() + 5000 > line.getY2()) {//Only draws lines in relatively small intervals
+                    graphics2D.draw(line);
+                }
             }
         }
+    }
+
+    public HashSet<Point> getXIntercepts() {
+        return RootType.X_INTERCEPT.getRoots(gui, this, gui.getMinDomain(), gui.getMaxDomain());
     }
 
     public HashSet<Point> getCriticalPoints() {
         return RootType.CRITICAL_POINT.getRoots(gui, this, gui.getMinDomain(), gui.getMaxDomain());
     }
 
-    public abstract String getFirstDerivative();
-
     public HashSet<Point> getInflectionPoints() {
         return RootType.INFLECTION_POINT.getRoots(gui, this, gui.getMinDomain(), gui.getMaxDomain());
     }
 
+    public abstract String getFirstDerivative();
+
     public abstract String getSecondDerivative();
 
     public abstract double getValueAt(double x, FunctionType functionType);
-    
-    public HashSet<Point> getXIntercepts() {
-        return RootType.X_INTERCEPT.getRoots(gui, this, gui.getMinDomain(), gui.getMaxDomain());
-    }
+
 }
