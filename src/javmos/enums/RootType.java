9fc0755c88596e633f533c9a21c073567a4d0fbd
Farhan Faruqui <farhanfaruqui13@gmail.com>
2019-05-02 00:50:56 -0400

    Fixed rounding error

--- src/javmos/enums/RootType.java
+++ src/javmos/enums/RootType.java
@@ -1,6 +1,7 @@
 package javmos.enums;
 
 import java.awt.Color;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.HashSet;
 import javmos.JavmosGUI;
@@ -34,6 +35,8 @@ public enum RootType {
     }
 
     public HashSet<Point> getRoots(JavmosGUI gui, Function function, double minDomain, double maxDomain) {
+        DecimalFormat thousandth = new DecimalFormat("#.###");
+        thousandth.setRoundingMode(RoundingMode.HALF_DOWN);
         HashSet<Point> roots = new HashSet<>();
         minDomain = -400 / gui.getZoom() * gui.getDomainStep();
         maxDomain = 400 / gui.getZoom() * gui.getDomainStep();
@@ -41,9 +44,9 @@ public enum RootType {
         for (double i = minDomain; i < maxDomain; i += 0.1) {
             if (newtonsMethod(function, i, ATTEMPTS) != null) {
                 if (rootName.equals("x-intercept")) {
-                    roots.add(new Point(gui, this, this.newtonsMethod(function, i, ATTEMPTS), 0.0));
+                    roots.add(new Point(gui, this, Double.parseDouble(thousandth.format(newtonsMethod(function, i, ATTEMPTS))), 0.0));
                 } else {
-                    roots.add(new Point(gui, this, this.newtonsMethod(function, i, ATTEMPTS), function.getValueAt(this.newtonsMethod(function, i, ATTEMPTS), FunctionType.ORIGINAL)));
+                    roots.add(new Point(gui, this, Double.parseDouble(thousandth.format(newtonsMethod(function, i, ATTEMPTS))), function.getValueAt(newtonsMethod(function, i, ATTEMPTS), FunctionType.ORIGINAL)));
                 }
             }
         }
@@ -51,15 +54,14 @@ public enum RootType {
     }
 
     protected Double newtonsMethod(Function function, double guess, int attempts) {
-        DecimalFormat thousandth = new DecimalFormat("#.###");
         double numerator = function.getValueAt(guess, functionOne);
         double denominator = function.getValueAt(guess, functionTwo);
         double ans = guess - (numerator / denominator);
         //null is returned if Newtons method is undefined or root not found, if new value - old value <= 0.000001 and root is in fact 0, a point has been converged on otherwise Newtons method is run again with new guess
         if (attempts == 0 || denominator == 0) {
             return null;
-        } else if (Math.abs((Math.abs(ans) - Math.abs(guess))) <= 0.000001 && function.getValueAt(ans, functionOne) <= 0.01 && function.getValueAt(ans, functionOne) >= -0.01) {
-            return Double.parseDouble(thousandth.format(ans));
+        } else if (Math.abs((Math.abs(ans) - Math.abs(guess))) <= 0.000001 && function.getValueAt(ans, functionOne) <= 0.000001 && function.getValueAt(ans, functionOne) >= -0.000001) {
+            return ans;
         } else {
             return newtonsMethod(function, ans, attempts - 1);
         }
